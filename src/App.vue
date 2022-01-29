f1aa100c2867c3f6a2eef63591ace5c8d8210535
ilham89 <ilham.kodingworks@gmail.com>
2022-01-29 13:10:17 +0700

    feat: ✨ initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <Logos />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logos from "./components/Logos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Logos,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Logos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Find best logos</h2>
+    <h2>Ilham Kukuh Prakosa</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logos",
+};
+</script>
+
+<style scoped></style>
